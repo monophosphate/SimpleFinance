8a1ef5f21fe907f50910b781dfda6e91b8cbf9d2
Alex Babou <ababou@dendev.net>
2021-05-03 21:44:23 -0600

    Initial Commit

---
+++ SimpleFinance/SimpleFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFinanceApp.swift
+//  SimpleFinance
+//
+//  Created by Alex on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
